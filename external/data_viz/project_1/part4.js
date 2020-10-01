b2cbdfb49a44fb487464f80837c54976fe9231e9
Yuhan Jiang <jiangyuh@hawaii.edu>
2020-09-30 16:20:16 -1000

    Update part4.js

--- external/data_viz/project_1/part4.js
+++ external/data_viz/project_1/part4.js
@@ -14,7 +14,7 @@ Plotly.d3.csv('Cholera/choleraPumpLocations.csv', function (err, data_pump) {
       ['0.8', '#CC1d1d'],
       ['1.0', '#EE0000']
     ];
-
+    console.log("Total Death: " + _.reduce(death_num, (a,b) => (a+b)))
 
 
     var data = [
@@ -23,7 +23,7 @@ Plotly.d3.csv('Cholera/choleraPumpLocations.csv', function (err, data_pump) {
         name: "Pump",
         lat: pump_latitude,
         lon: pump_longitude,
-        marker: { color: "#0088CC", size: 20 },
+        marker: { color: "#0099DD", size: 15 },
       },
       {
         type: "scattermapbox",
@@ -35,14 +35,14 @@ Plotly.d3.csv('Cholera/choleraPumpLocations.csv', function (err, data_pump) {
           color: death_num,
           colorscale: scale,
           reversescale: false,
-          opacity: 0.8,
-          size: _.map(death_num, (num) => (num + 6))}
+          opacity: 0.9,
+          size: _.map(death_num, (num) => (num + 7))}
       }
     ];
 
     var layout = {
       dragmode: "zoom",
-      mapbox: { style: "open-street-map", center: { lat: 51.513, lon: -0.137 }, zoom: 15 },
+      mapbox: { style: "stamen-terrain", center: { lat: 51.513, lon: -0.137 }, zoom: 15.5 },
       margin: { r: 0, t: 0, b: 0, l: 0 }
     };
 
